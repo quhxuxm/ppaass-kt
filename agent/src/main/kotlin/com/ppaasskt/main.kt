8f5086fcce60dd755531b0dd5e33398dab8a4a0f
quh <quhxuxm_quh@163.com>
2020-06-28 17:58:08 +0800

    initialize

---
+++ agent/src/main/kotlin/com/ppaasskt/main.kt
@@ -0,0 +1,11 @@
+package com.ppaasskt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PpaassMain;
+
+fun main(args: Array<String>) {
+    runApplication<PpaassMain>(*args)
+}
