2c68d3537e8ec0afcf94de43c2d5d0342a322185
quh <quhxuxm_quh@163.com>
2020-07-01 16:39:22 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.72"))
+    }
+}
+
 group = "com.ppaass.kt"
 version = "1.0-SNAPSHOT"
 
@@ -12,3 +23,11 @@ allprojects {
         mavenCentral();
     }
 }
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+    }
+}
+
