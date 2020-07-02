ae71f3ae4720510198ae24a38150654ce9758ca0
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2020-07-02 23:03:45 +0800

    init

--- proxy/src/main/kotlin/com/ppaass/kt/proxy/api/IProxy.kt
+++ proxy/src/main/kotlin/com/ppaass/kt/proxy/api/IProxy.kt
@@ -1,7 +1,6 @@
 package com.ppaass.kt.proxy.api
 
 interface IProxy {
-    fun init();
     fun start();
     fun stop();
 }
--- proxy/src/main/kotlin/com/ppaass/kt/proxy/impl/Proxy.kt
+++ proxy/src/main/kotlin/com/ppaass/kt/proxy/impl/Proxy.kt
@@ -1,7 +1,10 @@
 package com.ppaass.kt.proxy.impl
 
 import com.ppaass.kt.proxy.api.IProxy
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -13,14 +16,20 @@ class Proxy(proxyConfiguration: ProxyConfiguration) : IProxy {
     }
 
     private val masterThreadGroup: NioEventLoopGroup
+    private val workerThreadGroup: NioEventLoopGroup
+    private val serverBootstrap: ServerBootstrap
 
     init {
-        this.masterThreadGroup = NioEventLoopGroup(proxyConfiguration.masterIoEventThreadNumber);
-    }
-
-    override fun init() {
-        logger.debug("Begin to start proxy.")
-        TODO("Not yet implemented")
+        this.masterThreadGroup = NioEventLoopGroup(proxyConfiguration.masterIoEventThreadNumber)
+        this.workerThreadGroup = NioEventLoopGroup(proxyConfiguration.workerIoEventThreadNumber)
+        this.serverBootstrap = ServerBootstrap()
+        this.serverBootstrap.group(this.masterThreadGroup, this.workerThreadGroup)
+        this.serverBootstrap.channel(NioServerSocketChannel::class.java)
+        this.serverBootstrap.option(ChannelOption.SO_BACKLOG, proxyConfiguration.soBacklog)
+        this.serverBootstrap.option(ChannelOption.TCP_NODELAY, true)
+        this.serverBootstrap.option(ChannelOption.SO_REUSEADDR, true)
+        this.serverBootstrap.childOption(ChannelOption.TCP_NODELAY, true)
+        this.serverBootstrap.childHandler()
     }
 
     override fun start() {
