d802322456a8bed7233ffa403d4fd3f179e47d9f
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2020-07-04 22:15:40 +0800

    init

---
+++ proxy/src/main/kotlin/com/ppaass/kt/proxy/configuration.kt
@@ -0,0 +1,22 @@
+package com.ppaass.kt.proxy
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Service
+
+@ConfigurationProperties("ppaass.proxy")
+@Service
+class ProxyConfiguration {
+    var masterIoEventThreadNumber: Int = 0
+    var workerIoEventThreadNumber: Int = 0
+    var businessEventThreadNumber: Int = 0
+    var targetDataTransferIoEventThreadNumber: Int = 0
+    var soBacklog: Int = 0
+    var port: Int = 0
+    var targetConnectionTimeout: Int = 0
+    var agentConnectionIdleSeconds: Int = 0
+    var targetReceiveDataAverageBufferMinSize: Int = 0
+    var targetReceiveDataAverageBufferInitialSize: Int = 0
+    var targetReceiveDataAverageBufferMaxSize: Int = 0
+    var targetSoRcvbuf: Int = 0
+    var remainingBytesInProxyWriteBufferToPauseTargetAutoRead: Long = 0
+}
--- proxy/src/main/kotlin/com/ppaass/kt/proxy/proxy.kt
+++ proxy/src/main/kotlin/com/ppaass/kt/proxy/proxy.kt
@@ -21,28 +21,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
-
-@ConfigurationProperties("ppaass.proxy")
-@Service
-class ProxyConfiguration {
-    var masterIoEventThreadNumber: Int = 0
-    var workerIoEventThreadNumber: Int = 0
-    var businessEventThreadNumber: Int = 0
-    var targetDataTransferIoEventThreadNumber: Int = 0
-    var soBacklog: Int = 0
-    var port: Int = 0
-    var targetConnectionTimeout: Int = 0
-    var agentConnectionIdleSeconds: Int = 0
-    var targetReceiveDataAverageBufferMinSize: Int = 0
-    var targetReceiveDataAverageBufferInitialSize: Int = 0
-    var targetReceiveDataAverageBufferMaxSize: Int = 0
-    var targetSoRcvbuf: Int = 0
-    var remainingBytesInProxyWriteBufferToPauseTargetAutoRead: Long = 0
-}
 
 internal interface IProxy {
     fun start();
