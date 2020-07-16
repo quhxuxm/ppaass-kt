915fc24b27c2e49926336c86a802c115258ea4bc
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2020-07-16 11:21:12 +0800

    init

--- agent/src/main/kotlin/com/ppaass/kt/agent/handler/socks/v4/SocksV4Handler.kt
+++ agent/src/main/kotlin/com/ppaass/kt/agent/handler/socks/v4/SocksV4Handler.kt
@@ -1,7 +1,19 @@
 package com.ppaass.kt.agent.handler.socks.v4
 
 import com.ppaass.kt.agent.configuration.AgentConfiguration
-import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.SimpleChannelInboundHandler
+import io.netty.handler.codec.socksx.SocksMessage
+import io.netty.handler.codec.socksx.v4.Socks4CommandRequest
 
-class SocksV4Handler(private val agentConfiguration: AgentConfiguration) : ChannelInboundHandlerAdapter() {
+class SocksV4Handler(private val agentConfiguration: AgentConfiguration) : SimpleChannelInboundHandler<SocksMessage>() {
+    override fun channelRead0(agentChannelContext: ChannelHandlerContext, msg: SocksMessage) {
+        val channelPipeline = agentChannelContext.pipeline()
+        with(channelPipeline) {
+            when (msg) {
+                is Socks4CommandRequest -> {
+                }
+            }
+        }
+    }
 }
