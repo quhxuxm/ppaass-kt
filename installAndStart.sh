ebecb9f3433cc05860e58ae310a28f83ba7a7cc4
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2020-07-17 16:39:22 +0800

    init - use gradle

--- installAndStart.sh
+++ installAndStart.sh
@@ -1,21 +1,21 @@
 # wget https://github.com/quhxuxm/ppaass/raw/master/distribute/target/ppaass-1.0-SNAPSHOT.zip
-rm -rf /home/build/agent
-rm -rf /home/build/proxy
-rm /home/build/ppaass-1.0-SNAPSHOT.zip
+ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
+rm -rf /home/build
 rm -rf /home/sourcecode
 # Build
 mkdir /home/sourcecode
+mkdir /home/build
 cd /home/sourcecode
 git clone https://github.com/quhxuxm/ppaass-kt.git ppaass
 cd /home/sourcecode/ppaass
 git pull
-mvn clean package
-cp /home/sourcecode/ppaass/distribute/target/ppaass-1.0-SNAPSHOT.zip /home/build
+gradle
+ps -ef | grep gradle | grep -v grep | awk '{print $2}' | xargs kill -9
+cp /home/sourcecode/ppaass/proxy/build/distributions/proxy-boot.zip /home/build
 cd /home/build
-chmod 777 ppaass-1.0-SNAPSHOT.zip
-unzip ppaass-1.0-SNAPSHOT.zip
-chmod 777 /home/build/proxy/proxy-1.0-SNAPSHOT.jar
-chmod 777 /home/build/proxy/startProxy.sh
-cd /home/build/proxy
-./startProxy.sh
-ps -ef|grep java|grep proxy-1.0
+chmod 777 proxy-boot.zip
+unzip proxy-boot.zip
+cd /home/build/proxy-boot/bin
+nohup ./proxy >run.log 2>&1 &
+ps -ef|grep java|grep proxy
+~
