f1169de25bdee669de21a5f00fcaa0609339312c
quh <quhxuxm_quh@163.com>
2020-08-21 12:21:49 +0800

    adjust proxy thread number

--- agent/src/main/kotlin/com/ppaass/kt/agent/handler/socks/v5/SocksV5ProxyToAgentHandler.kt
+++ agent/src/main/kotlin/com/ppaass/kt/agent/handler/socks/v5/SocksV5ProxyToAgentHandler.kt
@@ -1,7 +1,6 @@
 package com.ppaass.kt.agent.handler.socks.v5
 
 import com.ppaass.kt.agent.configuration.AgentConfiguration
-import com.ppaass.kt.agent.handler.resourceClearHandler
 import com.ppaass.kt.common.exception.PpaassException
 import com.ppaass.kt.common.protocol.*
 import io.netty.buffer.Unpooled
@@ -36,11 +35,13 @@ internal class SocksV5ProxyToAgentHandler(private val agentChannel: Channel,
         if (agentChannel.pipeline()[SocksV5ConnectCommandHandler::class.java] != null) {
             agentChannel.pipeline().remove(SocksV5ConnectCommandHandler::class.java)
         }
-        if (!proxyChannelContext.channel().isActive) {
-            logger.error(
-                    "Fail to send connect message from agent to proxy because of proxy channel not active.")
-            throw PpaassException(
-                    "Fail to send connect message from agent to proxy because of proxy channel not active.")
+        agentChannel.pipeline().apply {
+            if (this[SocksV5ConnectCommandHandler::class.java] != null) {
+                remove(SocksV5ConnectCommandHandler::class.java)
+            }
+            addLast(
+                    SocksV5AgentToProxyHandler(proxyChannelContext.channel(),
+                            socks5CommandRequest, agentConfiguration))
         }
         proxyChannelContext.channel().writeAndFlush(agentMessage).addListener { future: Future<in Void?> ->
             if (!future.isSuccess) {
@@ -55,17 +56,9 @@ internal class SocksV5ProxyToAgentHandler(private val agentChannel: Channel,
                         "Fail to send connect message from agent to proxy because of exception.",
                         future.cause())
             }
-
-            val proxyChannel = proxyChannelContext.channel()
             logger.debug(
                     "Success connect to target server: {}:{}", socks5CommandRequest.dstAddr(),
                     socks5CommandRequest.dstPort())
-            agentChannel.pipeline().apply {
-                addLast(
-                        SocksV5AgentToProxyHandler(proxyChannel,
-                                socks5CommandRequest, agentConfiguration))
-                addLast(resourceClearHandler)
-            }
             agentChannel.writeAndFlush(DefaultSocks5CommandResponse(
                     Socks5CommandStatus.SUCCESS,
                     socks5CommandRequest.dstAddrType(),
