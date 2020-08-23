1eb7081d2952e5cbb86abb79d2ff2f31faf77488
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2020-08-23 13:35:39 +0800

    optimize jvm

--- agent/src/main/script/start.bat
+++ agent/src/main/script/start.bat
@@ -1,1 +1,1 @@
-javaw -jar -server -XX:+UseG1GC -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:+SegmentedCodeCache  -XX:+ExplicitGCInvokesConcurrent agent-1.0-SNAPSHOT.jar --spring.config.location=classpath:application.properties,file:./application.properties > run.log 2>&1 &
+start javaw -server -XX:+UseG1GC -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:+SegmentedCodeCache  -XX:+ExplicitGCInvokesConcurrent -jar agent-1.0-SNAPSHOT.jar --spring.config.location=classpath:application.properties,file:./application.properties > run.log 2>&1 &
