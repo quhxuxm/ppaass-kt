0f33fcb5944ba9280141a3031682e8ffb39f76ee
quh <quhxuxm_quh@163.com>
2020-08-24 16:19:04 +0800

    adjust proxy thread number

---
+++ agent/src/test/kotlin/com/ppaass/kt/test/Main.kt
@@ -0,0 +1,22 @@
+package com.ppaass.kt.test
+
+import org.apache.commons.io.IOUtils
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.net.URL
+
+fun main() {
+
+    val url = URL("https://www.youdao.com")
+    val proxyAddress = InetSocketAddress.createUnresolved("localhost", 10080)
+    (1..1000).forEach {
+        val connection = url.openConnection(Proxy(Proxy.Type.SOCKS, proxyAddress))
+        println("The $it time to access: ")
+        val inputStream = connection.getInputStream()
+        IOUtils.readLines(inputStream, Charsets.UTF_8).forEach {
+            println(it)
+        }
+        inputStream.close()
+        Thread.sleep(1000)
+    }
+}
