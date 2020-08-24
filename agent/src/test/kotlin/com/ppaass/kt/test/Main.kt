cab6cb4199767f083e317d3e84ce341bd0aa47ff
quh <quhxuxm_quh@163.com>
2020-08-24 17:10:43 +0800

    adjust proxy thread number

--- agent/src/test/kotlin/com/ppaass/kt/test/Main.kt
+++ agent/src/test/kotlin/com/ppaass/kt/test/Main.kt
@@ -4,19 +4,26 @@ import org.apache.commons.io.IOUtils
 import java.net.InetSocketAddress
 import java.net.Proxy
 import java.net.URL
+import java.util.concurrent.Executors
 
 fun main() {
 
     val url = URL("https://www.behance.net/")
     val proxyAddress = InetSocketAddress.createUnresolved("localhost", 10080)
-    (1..1000).forEach {
-        val connection = url.openConnection(Proxy(Proxy.Type.SOCKS, proxyAddress))
-        println("The $it time to access: ")
-        val inputStream = connection.getInputStream()
-        IOUtils.readLines(inputStream, Charsets.UTF_8).forEach {
-            println(it)
+    val threadPool = Executors.newFixedThreadPool(100)
+    while (true) {
+        threadPool.submit {
+            val connection = url.openConnection(Proxy(Proxy.Type.SOCKS, proxyAddress))
+            val startTime = System.currentTimeMillis()
+            println("The ${Thread.currentThread().name} start.")
+            val inputStream = connection.getInputStream()
+            IOUtils.readLines(inputStream, Charsets.UTF_8).forEach {
+                it.length
+            }
+            inputStream.close()
+            val endTime = System.currentTimeMillis()
+            println("The ${Thread.currentThread().name} end, use: ${endTime - startTime}ms.")
+            Thread.sleep(1000)
         }
-        inputStream.close()
-        Thread.sleep(1000)
     }
 }
