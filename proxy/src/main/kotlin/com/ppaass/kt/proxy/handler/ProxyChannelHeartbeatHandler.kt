2bbd5906fe3638ceafa8099d359c98d2a4ab6b35
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2020-10-26 20:16:14 +0800

    refact

--- proxy/src/main/kotlin/com/ppaass/kt/proxy/handler/ProxyChannelHeartbeatHandler.kt
+++ proxy/src/main/kotlin/com/ppaass/kt/proxy/handler/ProxyChannelHeartbeatHandler.kt
@@ -1,6 +1,10 @@
 package com.ppaass.kt.proxy.handler
 
-import io.netty.buffer.Unpooled
+import com.ppaass.kt.common.protocol.MessageBodyEncryptionType
+import com.ppaass.kt.common.protocol.ProxyMessage
+import com.ppaass.kt.common.protocol.ProxyMessageBody
+import com.ppaass.kt.common.protocol.ProxyMessageBodyType
+import com.ppaass.kt.common.protocol.generateUid
 import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
@@ -31,14 +35,27 @@ internal class ProxyChannelHeartbeatHandler : ChannelInboundHandlerAdapter() {
                 proxyChannelContext.channel().id().asLongText()
             }."
         }
-        proxyChannelContext.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener {
+        val heartBeatMessageBody = ProxyMessageBody(ProxyMessageBodyType.HEARTBEAT, generateUid())
+        val heartBeatMessage =
+            ProxyMessage(generateUid(), MessageBodyEncryptionType.random(), heartBeatMessageBody)
+        proxyChannelContext.writeAndFlush(heartBeatMessage).addListener {
             if (!it.isSuccess) {
+                logger.error {
+                    "Fail to write heartbeat message from proxy to agent, close the proxy channel. Proxy channel = ${
+                        proxyChannelContext.channel().id().asLongText()
+                    }"
+                }
                 proxyChannelContext.close()
                 return@addListener
             }
             val proxyChannel = proxyChannelContext.channel()
             val targetChannel = proxyChannel.attr(TARGET_CHANNEL).get()
             if (targetChannel == null) {
+                logger.error {
+                    "No target channel attached to proxy channel, close the proxy channel. Proxy channel = ${
+                        proxyChannelContext.channel().id().asLongText()
+                    }"
+                }
                 proxyChannelContext.close()
                 return@addListener
             }
