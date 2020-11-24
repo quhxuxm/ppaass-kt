24c9922efa57eeb3310ae8260e03b012494afca7
quh <quhxuxm_quh@163.com>
2020-11-24 16:05:53 +0800

    reconstruct

--- common/src/main/kotlin/com/ppaass/kt/common/Cryptography.kt
+++ common/src/main/kotlin/com/ppaass/kt/common/Cryptography.kt
@@ -1,5 +1,35 @@
 package com.ppaass.kt.common
 
-enum class EncryptionType {
-    AES, BLOWFISH
+/**
+ * The encryption type.
+ */
+enum class EncryptionType(private val value: Byte) {
+    /**
+     * The aes encryption
+     */
+    AES(0),
+
+    /**
+     * The blowfish encryption
+     */
+    BLOWFISH(1);
+
+    /**
+     * Get the value of the encryption type.
+     */
+    fun value(): Byte {
+        return this.value
+    }
+}
+
+/**
+ * Parse a encryption type form a given value.
+ */
+fun EncryptionType?.fromValue(value: Byte): EncryptionType? {
+    for (e in EncryptionType.values()) {
+        if (e.value() == value) {
+            return e
+        }
+    }
+    return null
 }
