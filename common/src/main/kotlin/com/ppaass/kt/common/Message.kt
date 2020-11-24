f1bb5c9237799b65e29da15fbcc29d7f7a0140d3
quh <quhxuxm_quh@163.com>
2020-11-24 15:59:54 +0800

    reconstruct

--- common/src/main/kotlin/com/ppaass/kt/common/Message.kt
+++ common/src/main/kotlin/com/ppaass/kt/common/Message.kt
@@ -10,18 +10,74 @@ interface MessageBodyType {
     fun value(): Byte
 }
 
+/**
+ * The message body
+ */
 class MessageBody<T>(
-    val id: String, val userToken: String, val targetAddress: String,
-    val targetPort: Int, val bodyType: T,
+    /**
+     * Message id
+     */
+    val id: String,
+    /**
+     * User token
+     */
+    val userToken: String,
+    /**
+     * Target hostname
+     */
+    val targetHost: String,
+    /**
+     * Target port
+     */
+    val targetPort: Int,
+    /**
+     * Message body type
+     */
+    val bodyType: T,
+    /**
+     * The data in bytes
+     */
     val data: ByteArray) where T : MessageBodyType, T : Enum<T>
 
-class Message<T>(val encryptionToken: String, val encryptionType: EncryptionType,
-                 val body: MessageBody<T>) where T : Enum<T>, T : MessageBodyType
+/**
+ * The message exchange between agent and proxy.
+ */
+class Message<T>(
+    /**
+     * The encryption token
+     */
+    val encryptionToken: String,
+    /**
+     * The encryption type
+     */
+    val encryptionType: EncryptionType,
+    /**
+     * The message body
+     */
+    val body: MessageBody<T>) where T : Enum<T>, T : MessageBodyType
 
+/**
+ * The agent message body type
+ */
 enum class AgentMessageBodyType(private val value: Byte) : MessageBodyType {
+    /**
+     * Create a connection on proxy and keep alive
+     */
     CONNECT_WITH_KEEP_ALIVE(0),
+
+    /**
+     * Create a connection on proxy and do not keep alive
+     */
     CONNECT_WITHOUT_KEEP_ALIVE(1),
+
+    /**
+     * Sending a TCP data
+     */
     TCP_DATA(2),
+
+    /**
+     * Sending a UDP data
+     */
     UDP_DATA(3);
 
     override fun value(): Byte {
@@ -30,15 +86,40 @@ enum class AgentMessageBodyType(private val value: Byte) : MessageBodyType {
 }
 
 enum class ProxyMessageBodyType(private val value: Byte) : MessageBodyType {
+    /**
+     * TCP data handled.
+     */
     OK_TCP(0),
+
+    /**
+     * UDP data handled.
+     */
     OK_UDP(1),
+
+    /**
+     * Heartbeat
+     */
     HEARTBEAT(2),
+
+    /**
+     * Connection fail
+     */
     CONNECT_FAIL(3),
+
+    /**
+     * Connection success
+     */
     CONNECT_SUCCESS(4);
 
     override fun value(): Byte {
         return this.value
     }
 }
+/**
+ * The type alias of agent message
+ */
 typealias AgentMessage = Message<AgentMessageBodyType>
+/**
+ * The type alias of proxy message
+ */
 typealias ProxyMessage = Message<ProxyMessageBodyType>
