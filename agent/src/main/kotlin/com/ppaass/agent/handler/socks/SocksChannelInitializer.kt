cdc6bef37837e93819d4dbaf2dbd089a79b264d0
quh <quhxuxm_quh@163.com>
2020-11-25 17:56:10 +0800

    reconstruct

--- agent/src/main/kotlin/com/ppaass/agent/handler/socks/SocksChannelInitializer.kt
+++ agent/src/main/kotlin/com/ppaass/agent/handler/socks/SocksChannelInitializer.kt
@@ -3,12 +3,15 @@ package com.ppaass.agent.handler.socks
 import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.socket.SocketChannel
+import io.netty.handler.codec.socksx.SocksPortUnificationServerHandler
 import org.springframework.stereotype.Service
 
 @ChannelHandler.Sharable
 @Service
 internal class SocksChannelInitializer : ChannelInitializer<SocketChannel>() {
-    override fun initChannel(ch: SocketChannel?) {
-        TODO("Not yet implemented")
+    override fun initChannel(agentChannel: SocketChannel) {
+        val  agentChannelPipeline=agentChannel.pipeline()
+        agentChannelPipeline.addLast(SocksPortUnificationServerHandler())
+        agentChannelPipeline.addLast(SAEntryHandler::class.java.getName(), saEntryHandler)
     }
 }
